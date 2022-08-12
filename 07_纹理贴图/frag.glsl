1c40d7f3a8e06fe55cf8400362c9a4575f126e2d
huanghuiqiang <1669903552@qq.com>
2022-08-12 19:50:52 +0800

    add: texture

--- 07_纹理贴图/frag.glsl
+++ 07_纹理贴图/frag.glsl
@@ -1,6 +1,7 @@
 precision mediump float;
-
+varying vec2 vTexCoord;
+uniform sampler2D uSampler;
 
 void main(){
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = texture2D(uSampler, vTexCoord);
 }
