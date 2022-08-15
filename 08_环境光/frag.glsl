87d7f051e04fdfede5eace89e2c202ca7892db2c
huanghuiqiang <1669903552@qq.com>
2022-08-15 14:39:45 +0800

    add ambient

---
+++ 08_环境光/frag.glsl
@@ -0,0 +1,9 @@
+precision mediump float;
+varying vec4 vColor;
+uniform vec3 uAmbientColor;
+uniform float uAmbientIntensity;
+
+void main(){
+  vec3 ambient = uAmbientColor * uAmbientIntensity;
+  gl_FragColor = vColor * vec4(ambient, 1.0);
+}
---
+++ 08_环境光/vert.glsl
@@ -0,0 +1,10 @@
+attribute vec3 aPosition;
+attribute vec4 aColor;
+varying vec4 vColor;
+uniform mat4 uMatrix;
+
+
+void main(){
+  vColor = aColor;
+  gl_Position = uMatrix * vec4(aPosition, 1.0);
+}
