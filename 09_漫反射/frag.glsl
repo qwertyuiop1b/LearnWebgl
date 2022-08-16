686a396d57c96a82dbeff01fa2faf0e940a698d8
huanghuiqiang <1669903552@qq.com>
2022-08-16 17:41:55 +0800

    add part diffuse

--- 09_漫反射/frag.glsl
+++ 09_漫反射/frag.glsl
@@ -1,4 +1,13 @@
 precision mediump float;
+varying vec4 vColor;
+
+uniform vec3 uAmbientColor;
+uniform float uAmbientIntensity;
+
+
 void main(){
-  
+  vec3 ambient = uAmbientColor * uAmbientIntensity;
+
+
+  gl_FragColor = vColor * vec4(ambient, 1.0);
 }
--- 09_漫反射/vert.glsl
+++ 09_漫反射/vert.glsl
@@ -1,3 +1,15 @@
-void main(){
+attribute vec3 aPosition;
+attribute vec4 aColor;
+varying vec4 vColor;
+uniform mat4 uMatrix;
 
+
+
+
+void main(){
+  vColor = aColor;
+  gl_Position = uMatrix * vec4(aPosition, 1.0);
+
+
+  // vec3 ==> vec2 vec3.xy
 }
